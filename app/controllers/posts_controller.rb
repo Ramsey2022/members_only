8617224295b2b0593057bd3daf0c3cdfb112668f
Ramsey2022 <abovethelotus@yahoo.com>
2023-07-13 14:41:33 -0700

    added user authentication

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+end
