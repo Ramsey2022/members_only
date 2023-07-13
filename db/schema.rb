6e8c5bb46efffb9e56644e6b1d02ef79080dcba6
Ramsey2022 <abovethelotus@yahoo.com>
2023-07-13 16:18:36 -0700

    added users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_13_211131) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_230021) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_211131) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
